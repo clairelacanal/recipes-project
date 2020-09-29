537f86811144822e1712082d1d398f64b57c573a
Claire Lacanal <lacanal.claire@gmail.com>
2020-09-29 20:34:14 +0200

    routes profil done

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router  = express.Router();
+const User = require('../models/User.model')
 
-router.get('/userProfile', (req, res) => {
+//Route d'affichage de mon userProfile
+router.get('/userProfile', (req, res, next) => {
   res.render("profile/profile-user", {
     userInSession: req.session.CurrentUser
   })
 })
+
+
+//Route d'affichage de mon account settings
+router.get('/userProfile/:id/accountSettings', (req, res, next) => {
+  res.render("profile/account-settings")
+})
+
+//Route de traitement de mon formulaire account settings
+router.post('/userProfile/:id/accountSettings', (req, res, next) => {
+  const {username, email, password} = req.body;
+
+  User.findByIdAndUpdate({
+    username,
+    email,
+    password,
+    photoUser: req.file.path
+  }).then(modifyAccountFromDB => {
+    newAccount: modifyAccountFromDB;
+  }).catch(err =>{
+    next(err);
+  })
+})
+
+
+
 
 
 
