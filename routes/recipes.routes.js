eaf65e3bfb04fe3a2db7b7100e1f978ecbdb11da
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-03 16:21:38 +0200

    route recipe

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Recipe = require('../models/Recipe.model');
+const Ingredient = require('../models/Ingredients.model');
 const fileUploader = require('../configs/cloudinary.config');
 
 // GET route pour afficher toutes les recettes
@@ -15,6 +16,21 @@ router.get('/recipes', (req,res,next) => {
     next(err)
   })
 })
+
+//POST route pour afficher les recettes en fonction des ingrédients choisis
+router.post('/recipes/search', (req,res,next) => {
+  const {ingredient1, ingredient2, ingredient3} = req.body;
+  console.log(ingredient1);
+
+  Ingredient.find({
+    ingredients: {$in:[ingredient1, ingredient2, ingredient3]}
+  })
+})
+
+
+
+///search?city=Barcelona&start-date=2018-01-18
+
 
 
 
