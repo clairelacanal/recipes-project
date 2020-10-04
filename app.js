27c51cbe140a3c6805c91ad24883dd134e26243a
Laura Bonavent <laura.bonavent@gmail.com>
2020-10-04 19:18:13 +0200

    nav bar in partials

--- app.js
+++ app.js
@@ -43,8 +43,11 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials("partials_absolute_path")
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 
 
 
