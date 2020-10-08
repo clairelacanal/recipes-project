312c12b017a97e135f063f4aa375b25005af8380
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-08 19:45:33 +0200

    modif en cours

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+function addFields() {
+  let li = document.createElement("LI");
+  var textnode = document.createTextNode("Water");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+}
